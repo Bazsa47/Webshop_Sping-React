8a619fe43b403468873f15f4e12972f3b7c18653
Bazsa47 <farkasbalazs0127@gmail.com>
2021-06-23 13:28:12 +0200

    Created User class

---
+++ Backend/webshop-backend/src/main/java/com/example/demo/user/User.java
@@ -0,0 +1,128 @@
+package com.example.demo.user;
+
+public class User {
+
+    private String userId;
+    private String userName;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String password;
+    private byte role;
+    private String profilePicture;
+    private String introduction;
+
+
+    public User() {
+    }
+
+    public User(String userName, String fullName, String email, String phone, String password, byte role, String profilePicture, String introduction) {
+        this.userName = userName;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.role = role;
+        this.profilePicture = profilePicture;
+        this.introduction = introduction;
+    }
+
+    public User(String userId, String userName, String fullName, String email, String phone, String password, byte role, String profilePicture, String introduction) {
+        this.userId = userId;
+        this.userName = userName;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.role = role;
+        this.profilePicture = profilePicture;
+        this.introduction = introduction;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public byte getRole() {
+        return role;
+    }
+
+    public void setRole(byte role) {
+        this.role = role;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", userName='" + userName + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                ", profilePicture='" + profilePicture + '\'' +
+                ", introduction='" + introduction + '\'' +
+                '}';
+    }
+}
